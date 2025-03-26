f30a1bdc89a62fdb428335a19a2c1ccc943674b1
Robert Stefanic <rstefanic72@gmail.com>
2025-03-25 22:50:12 -0400

    main: Add required `table` argument to program

--- main.go
+++ main.go
@@ -14,18 +14,25 @@ func main() {
 		name     string
 		user     string
 		password string
+		table    string
 	)
 
 	flag.StringVar(&host, "host", "127.0.0.1", "The host to connect to.")
 	flag.StringVar(&name, "name", "postgres", "Name of the database to connect to.")
 	flag.StringVar(&user, "user", "root", "The user/role in the DB to connect with.")
 	flag.StringVar(&password, "password", "", "The password of the user/role to connect with.")
+	flag.StringVar(&table, "table", "", "The table that you want to query for its schema.")
 	flag.Parse()
 
 	fmt.Println("host: ", host)
 	fmt.Println("name: ", name)
 	fmt.Println("user: ", user)
 	fmt.Println("pass: ", password)
+	fmt.Println("table: ", table)
+
+	if table == "" {
+		panic("argument \"table\" is required")
+	}
 
 	connString := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=disable", user, password, host, name)
 
