a10310295d290d7ba2ec494b8a534f5f4f931b09
Robert Stefanic <rstefanic72@gmail.com>
2025-04-14 23:06:15 -0400

    Test: Use seed to add deterministic output for Integration Test
    Now that we can pass in a seed argument when running the program, we can
    always expect the same output when running it with the integration test!

--- flake.nix
+++ flake.nix
@@ -32,6 +32,17 @@
       checks = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
+          expected = pkgs.lib.strings.concatStrings (
+            pkgs.lib.strings.intersperse "\n" [
+              "host:  postgres"
+              "name:  postgres"
+              "user:  postgres"
+              "pass:  "
+              "table:  todos"
+              "seed:  1"
+              "INSERT INTO todos (id,task,complete,created_at) VALUES (9172393864939720632,'Their turn have her formerly honour while Bismarckian example product.',false,'%d-%29d-%29d'),(424577099883569842,'My government not regularly children constantly judge turkey whoa what.',false,'%d-%25d-%25d'),(5560765190427624264,'Later include hand certain aircraft village skip his he yesterday.',true,'%d-%06d-%06d');\n"
+            ]
+          );
         in
         {
           default = pkgs.nixosTest {
@@ -46,7 +57,10 @@
               start_all()
               postgres.wait_for_open_port(5432)
 
-              client.succeed('dummy --host postgres --name postgres --user postgres --table todos --count 3')
+              expected = """${expected}"""
+              actual = client.succeed('dummy --host postgres --name postgres --user postgres --table todos --count 3 --seed 1')
+
+              assert expected == actual, "actual:\n" + actual + "\nexpected:\n" + expected
             '';
           };
         }
