4f4914495263a7d383abf45cb9dcd1d7dd524463
Robert Stefanic <rstefanic72@gmail.com>
2025-05-22 10:39:58 -0500

    Generate insert statements for mulitple tables
    Removed the `tableName` and `count` arguments to the program. For the
    time being, these will be completely defined in the YAML file since the
    count is specific to the table.
    
    If `count` is not specified for the table in the YAML file, then we'll
    default to creating 10 records. Perhaps the `count` program argument can
    be added back in to change the default count.

--- main.go
+++ main.go
@@ -15,15 +15,11 @@ import (
 
 func main() {
 	var (
-		path      string
-		tableName string
-		count     int
-		seed      int
+		path string
+		seed int
 	)
 
 	flag.StringVar(&path, "path", "dummy.yml", "Path to the configuration yaml file.")
-	flag.StringVar(&tableName, "table", "", "The table that you want to create data dummy for.")
-	flag.IntVar(&count, "count", 0, "The number of rows of dummy data to generate.")
 	flag.IntVar(&seed, "seed", rand.Int(), "Set the seeder used to generate the output.")
 	flag.Parse()
 
@@ -35,65 +31,59 @@ func main() {
 	var config Config
 	yaml.Unmarshal(configFile, &config)
 
-	// If there was no seed set in the config file, randomize it
+	// If there was no seed set in the config file, use the randomized one
 	if config.Options.Seed == 0 {
 		config.Options.Seed = seed
 	}
-
-	if tableName == "" && len(config.Tables) == 0 {
-		panic("argument \"table\" is required")
-	}
-
-	if tableName == "" {
-		tableName = config.Tables[0].Name
-	}
-
-	// Try to use the one from the config if we're missing the `count` program argument
-	if count == 0 {
-		count = config.Tables[0].Count
-	}
-
-	if count == 0 {
-		panic("\"count\" is 0 -- no data to generate")
-	}
-
-	var table = postgresql.NewTable(tableName)
-
 	gofakeit.Seed(config.Options.Seed)
-
-	psqlDb, err := postgresql.New(config.Server.User, config.Server.Password, config.Server.Host, config.Server.Name)
-	if err != nil {
-		panic("(postgresql.New): " + err.Error())
-	}
-	defer psqlDb.Close()
-
-	columns, err := psqlDb.GetTableColumns(tableName)
-	if err != nil {
-		panic("(postgresql.GetTableData): " + err.Error())
-	}
-	table.Columns = columns
-
-	err = table.FillMetadata()
-	if err != nil {
-		panic(err)
-	}
-
-	err = table.CreateData(count)
-	if err != nil {
-		panic(err)
-	}
 
 	if !config.Options.HideInputComment {
 		fmt.Println("-- host:", config.Server.Host)
 		fmt.Println("-- name:", config.Server.Name)
 		fmt.Println("-- user:", config.Server.User)
 		fmt.Println("-- pass:", config.Server.Password)
-		fmt.Println("-- table:", tableName)
 		fmt.Println("-- seed:", config.Options.Seed)
 		fmt.Println("")
 	}
 
-	fmt.Println(table.ToPsqlStatement())
+	for i, table := range config.Tables {
+		if i > 0 {
+			fmt.Print("\n\n")
+		}
+		var t = postgresql.NewTable(table.Name)
+
+		psqlDb, err := postgresql.New(config.Server.User, config.Server.Password, config.Server.Host, config.Server.Name)
+		if err != nil {
+			panic("(postgresql.New): " + err.Error())
+		}
+		defer psqlDb.Close()
+
+		columns, err := psqlDb.GetTableColumns(t.Name)
+		if err != nil {
+			panic("(postgresql.GetTableData): " + err.Error())
+		}
+		t.Columns = columns
+
+		err = t.FillMetadata()
+		if err != nil {
+			panic(err)
+		}
+
+		// Set the defualt count to 10 if it isn't set
+		var count int
+		if table.Count == 0 {
+			count = 10
+		} else {
+			count = table.Count
+		}
+
+		err = t.CreateData(count)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(t.ToPsqlStatement())
+	}
 }
 
 type Config struct {
